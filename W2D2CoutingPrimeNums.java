148b45fc9fce5513c754c0643683bd42015160e5
Mr. Developer <31615147+Renukacharya@users.noreply.github.com>
2021-12-24 13:46:48 +0530

    Add files via upload

---
+++ W2D2CoutingPrimeNums.java
@@ -0,0 +1,33 @@
+package Startithere;
+
+public class W2D2CoutingPrimeNums {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int a[]={2,3,4,5,6,7,8};
+		    int count=0;
+		   for(int i=0;i<a.length;i++)
+		   {
+		     if(isPrime(a[i]))
+		      count++;
+		   }
+		  System.out.println(count);
+		 }
+		 public static boolean isPrime(int n){
+		   boolean flag=true;
+		    if(n==2)
+		      return flag;
+		    else{
+		   for(int i=2;i<=n/2;i++)
+		   {
+		      if(n%i==0)
+		       {
+		         flag=false;
+		         break;
+		       }
+		   }
+		    return flag;
+}
+
+}
+}
