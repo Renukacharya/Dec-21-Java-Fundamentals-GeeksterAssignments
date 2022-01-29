1a99006561709cff6261d2e9c781dbb91363ff07
Mr. Developer <31615147+Renukacharya@users.noreply.github.com>
2022-01-29 21:39:38 +0530

    Add files via upload

---
+++ Jan25AllRepeatedCharsOfString.java
@@ -0,0 +1,27 @@
+package itsJanuary2022;
+
+public class Jan25AllRepeatedCharsOfString { static final int NO_OF_CHARS = 256;
+
+static void fillCharCounts(String str, int[] count)
+{
+    for (int i = 0; i < str.length(); i++)
+        count[str.charAt(i)]++;
+}
+
+static void printDuplicates(String str)
+{
+    int count[] = new int[NO_OF_CHARS];
+    fillCharCounts(str, count);
+
+    for (int i = 0; i < NO_OF_CHARS; i++)
+        if (count[i] > 1)
+            System.out.println((char)(i) + " = " + count[i]);
+}
+
+// Driver Method
+public static void main(String[] args)
+{
+    String str = "CharyaIs Here";
+    printDuplicates(str);
+}
+}
